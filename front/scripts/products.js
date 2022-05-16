b8ef97c66ef8dd2bcb8a56bc3e6471693c526837
m4d <julien.conforto@gmail.com>
2022-05-16 10:38:20 +0200

    colors display on product page

--- front/scripts/products.js
+++ front/scripts/products.js
@@ -8,6 +8,7 @@
 
 function getArticleId() {
     return new URL(location.href).searchParams.get('id');
+    addBasket(article);
 }
 
 function getArticle(articleId) {
@@ -30,5 +31,12 @@ function hydrateArticle(article) {
     document.getElementById('title').textContent = article.name;
     document.getElementById('description').textContent = article.description;
     document.getElementById('price').textContent = article.price;
-    document.querySelector('colors').textContent = article.colors;
+    document.getElementById('colors').textContent = article.colors;
+    for (let colors of article.colors) {
+        console.table(colors);
+        let productColors = document.createElement('option');
+        document.querySelector('#colors').appendChild(productColors);
+        productColors.value = colors;
+        productColors.innerHTML = colors;
+    }
 }
