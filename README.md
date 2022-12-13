6da85a4b6ede6013e6de37e4f3edf2e8aca2e378
m4d <julien.conforto@gmail.com>
2022-12-13 15:40:08 +0100

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Kanap
+
+## About / Synopsis
+
+- Kanap is a website that alloxs users to choose their couch, color and add it to cart
+- Project status: will be validate by Openclassrooms
+
+### Content
+
+this project uses HTML, CSS and JS.
+The HTML and CSS are already here when the project begin, we just had to addd the javascript code.
+It is mostly a work about javascript.
+
+### Back end Prerequisites
+
+You will need to have Node and `npm` installed locally on your machine.
+
+### Back end Installation
+
+Clone this repo. From the "back" folder of the project, run `npm install`. You
+can then run the server with `node server`.
+The server should run on `localhost` with default port `3000`. If the
+server runs on another port for any reason, this is printed to the
+console when the server starts, e.g. `Listening on port 3001`.
--- front/scripts/products.js
+++ front/scripts/products.js
@@ -1,11 +1,11 @@
 // taking product ID from URL
 var str = window.location.href;
 var url = new URL(str);
-const idProduct = url.searchParams.get("id");
+const idProduct = url.searchParams.get('id');
 
 // On vérifie que la page actuelle est bien la page produit afin d'executer le code principale uniquement lorsque c'est la bonne page.
 // Cela nous permet d’éviter les erreurs lorsqu'on importe les fonctions exportés ici dans d'autre fichiers js
-if (url.pathname.includes("product.html")) {
+if (url.pathname.includes('product.html')) {
   console.log(idProduct);
   getArticle(idProduct).then((article) => getPost(article));
 }
@@ -14,7 +14,7 @@ if (url.pathname.includes("product.html")) {
 export default async function getArticle(idProduct) {
   if (idProduct !== undefined) {
     // requête l'api avec l'idProduct présent dans l'url
-    return await fetch("http://localhost:3000/api/products/" + idProduct)
+    return await fetch('http://localhost:3000/api/products/' + idProduct)
       // passe la réponse au format json
       .then(function (res) {
         if (res.ok) {
@@ -27,35 +27,35 @@ export default async function getArticle(idProduct) {
         return article;
       })
       .catch((error) => {
-        console.log("Erreur de la requête API");
+        console.log('Erreur de la requête API');
       });
   }
 }
 
 function getPost(article) {
   // Getting article image
-  let productImg = document.createElement("img");
-  document.querySelector(".item__img").appendChild(productImg);
+  let productImg = document.createElement('img');
+  document.querySelector('.item__img').appendChild(productImg);
   productImg.src = article.imageUrl;
   productImg.alt = article.altTxt;
 
   // Article title
-  let productName = document.getElementById("title");
+  let productName = document.getElementById('title');
   productName.innerHTML = article.name;
 
   // Article price
-  let productPrice = document.getElementById("price");
+  let productPrice = document.getElementById('price');
   productPrice.innerHTML = article.price;
 
   // Article description
-  let productDescription = document.getElementById("description");
+  let productDescription = document.getElementById('description');
   productDescription.innerHTML = article.description;
 
   // Loop to take colors
   for (let colors of article.colors) {
     console.table(colors);
-    let productColors = document.createElement("option");
-    document.querySelector("#colors").appendChild(productColors);
+    let productColors = document.createElement('option');
+    document.querySelector('#colors').appendChild(productColors);
     productColors.value = colors;
     productColors.innerHTML = colors;
   }
@@ -63,11 +63,11 @@ function getPost(article) {
 }
 // add basket function
 function addToCart(article) {
-  const btn_envoyerPanier = document.querySelector("#addToCart");
+  const btn_envoyerPanier = document.querySelector('#addToCart');
   // Basket button with condition (article quantity must be between 1 and 100)
-  btn_envoyerPanier.addEventListener("click", (event) => {
-    const colorPicked = document.querySelector("#colors");
-    const quantityPicked = document.querySelector("#quantity");
+  btn_envoyerPanier.addEventListener('click', (event) => {
+    const colorPicked = document.querySelector('#colors');
+    const quantityPicked = document.querySelector('#quantity');
     if (
       quantityPicked.value > 0 &&
       quantityPicked.value <= 100 &&
@@ -90,9 +90,9 @@ function addToCart(article) {
       };
 
       // Local storage initialisation
-      let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
+      let produitLocalStorage = JSON.parse(localStorage.getItem('produit'));
 
-      // Pop up to confirm choice and redirect to cart.html
+      // Pop up to confirm choice
       const popupConfirmation = () => {
         if (
           window.confirm(`Votre commande de ${choixQuantite} ${article.name} ${colorChoice} est ajoutée au panier
@@ -112,13 +112,13 @@ function addToCart(article) {
             parseInt(optionsProduit.quantiteProduit) +
             parseInt(resultFind.quantiteProduit);
           resultFind.quantiteProduit = newQuantite;
-          localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+          localStorage.setItem('produit', JSON.stringify(produitLocalStorage));
           console.table(produitLocalStorage);
           popupConfirmation();
           // If the product is not in the basket
         } else {
           produitLocalStorage.push(optionsProduit);
-          localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+          localStorage.setItem('produit', JSON.stringify(produitLocalStorage));
           console.table(produitLocalStorage);
           popupConfirmation();
         }
@@ -126,7 +126,7 @@ function addToCart(article) {
       } else {
         produitLocalStorage = [];
         produitLocalStorage.push(optionsProduit);
-        localStorage.setItem("produit", JSON.stringify(produitLocalStorage));
+        localStorage.setItem('produit', JSON.stringify(produitLocalStorage));
         console.table(produitLocalStorage);
         popupConfirmation();
       }
